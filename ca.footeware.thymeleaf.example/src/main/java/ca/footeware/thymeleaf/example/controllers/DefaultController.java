420cb66cd9115cd72133e611bab27463858a8580
craig <craig@notebook>
2017-02-17 19:58:21 -0500

    Add post thru url

--- ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/controllers/DefaultController.java
+++ ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/controllers/DefaultController.java
@@ -5,6 +5,7 @@ package ca.footeware.thymeleaf.example.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,5 +27,11 @@ public class DefaultController {
 	@ResponseBody
 	public String getWeather() {
 		return "Sunny!";
+	}
+
+	@RequestMapping(value = "/weather/{setting}", method = RequestMethod.POST)
+	@ResponseBody
+	public String setWeather(@PathVariable("setting") String setting) {
+		return setting;
 	}
 }
--- ca.footeware.thymeleaf.example/src/main/resources/static/js/main.js
+++ ca.footeware.thymeleaf.example/src/main/resources/static/js/main.js
@@ -1,9 +1,20 @@
 /**
- * 
+ * A simple GET and element update.
  */
 function getWeather() {
 	$.ajax("http://localhost:8080/weather").done(function(data) {
 		$("#weather").html(data);
+	}).fail(function(xhr, textStatus, errorThrown) {
+		alert(textStatus);
+	});
+}
+
+/**
+ * Post a value in the URL rather than using RequestBody.
+ */
+function setWeather() {
+	$.post("http://localhost:8080/weather/snow").done(function(data) {
+		$("#setting").html("You want " + data);
 	}).fail(function(xhr, textStatus, errorThrown) {
 		alert(textStatus);
 	});
