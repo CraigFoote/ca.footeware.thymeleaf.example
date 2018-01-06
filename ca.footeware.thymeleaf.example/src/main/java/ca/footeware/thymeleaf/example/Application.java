4b79a336fa3be221da449276266601e6b8490ddc
craigfoote <CraigFoote@gmail.com>
2018-01-05 21:08:14 -0500

    added file logging

--- ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/Application.java
+++ ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/Application.java
@@ -1,12 +1,24 @@
 package ca.footeware.thymeleaf.example;
 
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
+	private static final Logger LOG = LoggerFactory.getLogger(Application.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		LOG.info("Application started.");
+	}
+
+	@PreDestroy
+	protected void preDestroy() {
+		LOG.info("Application stopped.");
 	}
 }
--- ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/controllers/DefaultController.java
+++ ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/controllers/DefaultController.java
@@ -3,6 +3,8 @@
  */
 package ca.footeware.thymeleaf.example.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,27 +19,33 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DefaultController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(DefaultController.class);
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String getGreeting(Model model) {
 		model.addAttribute("greeting", "Hello World!");
+		LOG.info("Returning greeting to index");
 		return "index";
 	}
 
 	@RequestMapping(value = "/weather", method = RequestMethod.GET)
 	@ResponseBody
 	public String getWeather() {
+		LOG.info("Returning 'snow'.");
 		return "Snow!";
 	}
 
 	@RequestMapping(value = "/weather/{forecast}", method = RequestMethod.POST)
 	@ResponseBody
 	public String setWeather(@PathVariable("forecast") String setting) {
+		LOG.info("Received " + setting);
 		return setting;
 	}
 
 	@RequestMapping(value = "/weather/{forecast}", method = RequestMethod.DELETE)
 	@ResponseBody
 	public void deleteWeather(@PathVariable("forecast") String setting) {
+		LOG.info("Deleting " + setting);
 		return;
 	}
 }
