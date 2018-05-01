5ad7876ebf12464a7b97860175d28bd02aaea799
craig <craig@notebook>
2018-04-30 21:01:04 -0400

    upgrade to Spring Boot 2.0.1

--- ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/controllers/DefaultController.java
+++ ca.footeware.thymeleaf.example/src/main/java/ca/footeware/thymeleaf/example/controllers/DefaultController.java
@@ -7,9 +7,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -21,28 +22,28 @@ public class DefaultController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(DefaultController.class);
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@GetMapping("/")
 	public String getGreeting(Model model) {
 		model.addAttribute("greeting", "Hello World!");
 		LOG.info("Returning greeting to index");
 		return "index";
 	}
 
-	@RequestMapping(value = "/weather", method = RequestMethod.GET)
+	@GetMapping("/weather")
 	@ResponseBody
 	public String getWeather() {
 		LOG.info("Returning 'snow'.");
 		return "Snow!";
 	}
 
-	@RequestMapping(value = "/weather/{forecast}", method = RequestMethod.POST)
+	@PostMapping("/weather/{forecast}")
 	@ResponseBody
 	public String setWeather(@PathVariable("forecast") String setting) {
 		LOG.info("Received " + setting);
 		return setting;
 	}
 
-	@RequestMapping(value = "/weather/{forecast}", method = RequestMethod.DELETE)
+	@DeleteMapping("/weather/{forecast}")
 	@ResponseBody
 	public void deleteWeather(@PathVariable("forecast") String setting) {
 		LOG.info("Deleting " + setting);
